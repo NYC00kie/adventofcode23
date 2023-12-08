35973974cefcd7feadc0a057c49aa0585a1cb42e
Nici <48388532+NYC00kie@users.noreply.github.com>
2023-12-08 21:56:18 +0100

    zum schauen

--- 8/parttwo.py
+++ 8/parttwo.py
@@ -2,15 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy, random, re, math, collections
 
+def compute_gcd(x, y):
+
+   while(y):
+       x, y = y, x % y
+   return x
+
+# This function computes LCM
+def compute_lcm(x, y):
+   lcm = (x*y)//compute_gcd(x,y)
+   return lcm
 
 
 
 def main():
-	data = """LLR
+	data = """LR
 
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)"""
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"""
 	with open("./input.txt","r") as fs:
 		data = fs.read()
 
@@ -21,9 +36,13 @@ ZZZ = (ZZZ, ZZZ)"""
 
 	lettmap = {}
 
+	locations = []
+
 	for line in mapping:
 		
 		location = line.split(" = ")[0]
+		if location[2] == "A":
+			locations.append(location)
 		destination = line.split(" = ")[1].split(", ")
 		destinationL = destination[0][-3:]
 		destinationR = destination[1][:3]
@@ -31,14 +50,40 @@ ZZZ = (ZZZ, ZZZ)"""
 
 		lettmap[location] = {"L":destinationL,"R":destinationR}
 
-	i = 0
-	location = "AAA"
-	while location != "ZZZ":
-		location = lettmap[location][instructions[i%len(instructions)]]
-
-		i += 1
-
-	print(i)
 	
+
+	lengths = []
+
+	for loc in locations:
+		print(loc)
+		location = loc
+		i = 0
+		location = lettmap[location][instructions[i%len(instructions)]]
+		print(location)
+		i += 1
+		# findet keine wiederholung
+		while location != loc:
+			location = lettmap[location][instructions[i%len(instructions)]]
+
+			if i % 1000000 == 0:
+				# :3
+				print(i)
+
+			i += 1
+
+		lengths.append(i)
+		print(i)
+
+
+	res = 0
+
+	for i in lengths:
+		for j in lengths:
+			lcm = compute_lcm(i,j)
+			if lcm > res:
+				res = lcm
+	
+	print(res)
+
 if __name__ == '__main__':
 	main()
