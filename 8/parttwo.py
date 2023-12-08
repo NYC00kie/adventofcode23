faba24a2ccb33e5a7a825bbafdc05acfbf737b81
Nici <48388532+NYC00kie@users.noreply.github.com>
2023-12-08 18:34:35 +0100

    part one Day 8

--- 8/parttwo.py
+++ 8/parttwo.py
@@ -6,25 +6,39 @@ import scipy, random, re, math, collections
 
 
 def main():
-	data = """RL
+	data = """LLR
 
-AAA = (BBB, CCC)
-BBB = (DDD, EEE)
-CCC = (ZZZ, GGG)
-DDD = (DDD, DDD)
-EEE = (EEE, EEE)
-GGG = (GGG, GGG)
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)"""
 	with open("./input.txt","r") as fs:
 		data = fs.read()
 
 	instructions = data.split("\n\n")[0]
+	mapping = data.split("\n\n")[1].split("\n")
+
+	print(len(instructions))
+
+	lettmap = {}
+
+	for line in mapping:
+		
+		location = line.split(" = ")[0]
+		destination = line.split(" = ")[1].split(", ")
+		destinationL = destination[0][-3:]
+		destinationR = destination[1][:3]
 
 
+		lettmap[location] = {"L":destinationL,"R":destinationR}
 
+	i = 0
+	location = "AAA"
+	while location != "ZZZ":
+		location = lettmap[location][instructions[i%len(instructions)]]
 
+		i += 1
 
-
+	print(i)
 	
 if __name__ == '__main__':
 	main()
